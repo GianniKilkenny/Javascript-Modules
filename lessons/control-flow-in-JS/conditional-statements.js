5c32d4b7cd9261ba3ddf1e2d27b22b1ee2f1dea3
GianniKilkenny <giannikilkenny22404@gmail.com>
2025-07-15 10:58:02 -0700

    conditional-statements

---
+++ lessons/control-flow-in-JS/conditional-statements.js
@@ -0,0 +1,17 @@
+// if statement
+
+// let temp = 15; // number that is 30
+
+// if (temp > 25) {
+//   console.log("It is a hot day so stay hydrated bitch");
+// }
+
+const mark = 70;
+
+if (mark >= 90) {
+  console.log("You old as fuck bra");
+} else if (mark >= 80) {
+  console.log("You younger than yo homie but you still old as shit");
+} else if (mark >= 70) {
+  console.log("You getting up there lil bro");
+}
