70664df2a94baead531a807ec6d85329b518903d
GianniKilkenny <giannikilkenny22404@gmail.com>
2025-07-18 15:49:48 -0700

    maps and filters

--- lessons/arrays-&-objects/arrays.js
+++ lessons/arrays-&-objects/arrays.js
@@ -1,35 +1,46 @@
-const fruits = ["Apple", "Banana", "Cherry"];
-console.log(fruits);
-// console.log(fruits[0]);
-// console.log(fruits[2]);
+// const fruits = ["Apple", "Banana", "Cherry"];
+// console.log(fruits);
+// // console.log(fruits[0]);
+// // console.log(fruits[2]);
 
-// console.log(fruits[1]);
+// // console.log(fruits[1]);
 
-// fruits[1] = "blueberry";
+// // fruits[1] = "blueberry";
 
+// // console.log(fruits);
+
+// // console.log(fruits.length);
+
+// // push - adds to the end
+// fruits.push("Mango");
 // console.log(fruits);
 
-// console.log(fruits.length);
+// // pop - removes from the end
+// fruits.pop();
+// console.log(fruits);
 
-// push - adds to the end
-fruits.push("Mango");
-console.log(fruits);
+// // shift adds to beginning
+// fruits.shift();
+// console.log(fruits);
 
-// pop - removes from the end
-fruits.pop();
-console.log(fruits);
+// // unshift removes from beginning
+// fruits.unshift("Grape");
+// console.log(fruits);
 
-// shift adds to beginning
-fruits.shift();
-console.log(fruits);
+// // splice - add / remove elements
+// fruits.splice(1, 1, "Peach");
+// console.log(fruits);
 
-// unshift removes from beginning
-fruits.unshift("Grape");
-console.log(fruits);
+// fruits.splice(1, 2);
+// console.log(fruits);
 
-// splice - add / remove elements
-fruits.splice(1, 1, "Peach");
-console.log(fruits);
+const numbers = [1, 2, 3, 4, 5];
 
-fruits.splice(1, 2);
-console.log(fruits);
+// map
+console.log("numbers =", numbers);
+const doubled = numbers.map((num) => num * 2);
+console.log("doubled =", doubled);
+
+// filter
+const evenNumbers = numbers.filter((num) => num % 2 === 0);
+console.log(evenNumbers);
