520d639b74824562c1b18fc095a7b7c5032024b4
GianniKilkenny <giannikilkenny22404@gmail.com>
2025-07-18 15:32:36 -0700

    push, pop, shift, and unshift

---
+++ lessons/arrays-&-objects/arrays.js
@@ -0,0 +1,24 @@
+const fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[2]);
+
+// console.log(fruits[1]);
+
+// fruits[1] = "blueberry";
+
+// console.log(fruits);
+
+// console.log(fruits.length);
+
+fruits.push("Mango");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+fruits.unshift("Grapes");
+console.log(fruits);
