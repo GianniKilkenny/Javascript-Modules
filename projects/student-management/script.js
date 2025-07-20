1d8f4db1936573104f80a74180fc4de1e2225127
GianniKilkenny <giannikilkenny22404@gmail.com>
2025-07-20 10:38:56 -0700

    filter and map added, tested code and it worked

--- projects/student-management/script.js
+++ projects/student-management/script.js
@@ -20,6 +20,30 @@ function removeStudent(name) {
 }
 
 // function to filter students
-function topStudents(name);{
-
+function topStudents(minGrade) {
+  return students.filter((student) => student.grade >= minGrade);
 }
+
+// function to map students in formatted list
+function formatStudentList() {
+  return students.map((student) => `${student.name} - Grade: ${student.grade}`);
+}
+
+// start
+console.log("students =", students);
+
+// add students
+addStudent("Gianni", 90);
+addStudent("emily", 85);
+addStudent("picos", 100);
+addStudent("sam", 70);
+addStudent("blueface", 50);
+
+console.log("students =", students);
+
+console.log(formatStudentList());
+
+console.log(students);
+
+console.log("Students with A's");
+console.log(topStudents(80));
